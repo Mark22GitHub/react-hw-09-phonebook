46ead3bb93180fcbf274db5b679757bf6f28e399
Mark22GitHub <atelezhenko93@gmail.com>
2021-03-28 02:26:52 +0200

    Components on hooks +

--- src/Components/ContactList/ContactList.js
+++ src/Components/ContactList/ContactList.js
@@ -1,11 +1,27 @@
-import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import styles from "./ContactList.module.css";
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import styles from './ContactList.module.css';
 
-import { contactsOperations, contactsSelectors } from "../../redux/contacts";
+import { contactsOperations, contactsSelectors } from '../../redux/contacts';
 
-const ContactList = ({ contacts, onDeleteFromContacts }) => {
+const ContactList = () => {
+  const dispatch = useDispatch();
+
+  const contacts = useSelector(contactsSelectors.getVisibleContacts);
+
+  const onDeleteFromContacts = useCallback(
+    id => {
+      dispatch(contactsOperations.deleteFromContacts(id));
+    },
+    [dispatch],
+  );
+
+  // const onDeleteFromContacts = id => {
+  //   dispatch(contactsOperations.deleteFromContacts(id));
+  // };
+
   return (
     <div className={styles.container}>
       <ul>
@@ -27,25 +43,61 @@ const ContactList = ({ contacts, onDeleteFromContacts }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  contacts: contactsSelectors.getVisibleContacts(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  onDeleteFromContacts: (id) =>
-    dispatch(contactsOperations.deleteFromContacts(id)),
-});
-
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
-    })
-  ).isRequired,
+    }),
+  ),
 
   onDeleteFromContacts: PropTypes.func,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
+export default ContactList;
+// ===================================================================
+// const ContactList = ({ contacts, onDeleteFromContacts }) => {
+//   return (
+//     <div className={styles.container}>
+//       <ul>
+//         {contacts.map(({ id, name, number }) => (
+//           <li key={id} className={styles.ContactListItem}>
+//             <p className={styles.ContactsListText}>
+//               {name} : {number}
+//             </p>
+//             <button
+//               className={styles.ContactsListButton}
+//               onClick={() => onDeleteFromContacts(id)}
+//             >
+//               Delete
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// const mapStateToProps = state => ({
+//   contacts: contactsSelectors.getVisibleContacts(state),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   onDeleteFromContacts: id =>
+//     dispatch(contactsOperations.deleteFromContacts(id)),
+// });
+
+// ContactList.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     }),
+//   ).isRequired,
+
+//   onDeleteFromContacts: PropTypes.func,
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
--- src/pages/contactsPage/ContactsPage.js
+++ src/pages/contactsPage/ContactsPage.js
@@ -1,37 +1,61 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import ContactForm from "../../Components/ContactForm/";
-import ContactList from "../../Components/ContactList/";
-import Filter from "../../Components/Filter/";
+import React, { Component, useEffect } from 'react';
+// import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import ContactForm from '../../Components/ContactForm/';
+import ContactList from '../../Components/ContactList/';
+import Filter from '../../Components/Filter/';
 
-import { contactsOperations, contactsSelectors } from "../../redux/contacts";
+import { contactsOperations, contactsSelectors } from '../../redux/contacts';
+
+const ContactsPage = () => {
+  const dispatch = useDispatch();
+
+  const isLoadingContacts = useSelector(contactsSelectors.getLoadingContacts);
+
+  useEffect(() => {
+    dispatch(contactsOperations.fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+      {isLoadingContacts && <h1>Loading...</h1>}
+    </div>
+  );
+};
+
+export default ContactsPage;
 
 // =============================ContactsPage=============================================
-class ContactsPage extends Component {
-  componentDidMount() {
-    this.props.fetchContacts();
-  }
+// class ContactsPage extends Component {
+//   componentDidMount() {
+//     this.props.fetchContacts();
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-        {this.props.isLoadingContacts && <h1>Loading...</h1>}
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h1>Phonebook</h1>
+//         <ContactForm />
+//         <h2>Contacts</h2>
+//         <Filter />
+//         <ContactList />
+//         {this.props.isLoadingContacts && <h1>Loading...</h1>}
+//       </div>
+//     );
+//   }
+// }
 
-const mapStateToProps = (state) => ({
-  isLoadingContacts: contactsSelectors.getLoadingContacts(state),
-});
+// const mapStateToProps = (state) => ({
+//   isLoadingContacts: contactsSelectors.getLoadingContacts(state),
+// });
 
-const mapDispatchToProps = (dispatch) => ({
-  fetchContacts: () => dispatch(contactsOperations.fetchContacts()),
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   fetchContacts: () => dispatch(contactsOperations.fetchContacts()),
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ContactsPage);
+// export default connect(mapStateToProps, mapDispatchToProps)(ContactsPage);
